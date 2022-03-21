d5d2ae091d5b1ad5940731684460a6a2b0d470f3
ajcastro <ajcastro29@gmail.com>
2022-03-21 22:03:11 +0800

    Hide dark theme toggle button

--- resources/views/panels/navbar-menu.blade.php
+++ resources/views/panels/navbar-menu.blade.php
@@ -1,6 +1,6 @@
 <ul class="nav navbar-nav align-items-center ms-auto">
   @each('panels.navbar-submenu', App\Models\Menu::tree(), 'menu')
 
-  <li class="nav-item d-none d-lg-block"><a class="nav-link nav-link-style"><i class="ficon"
-        data-feather="{{ $configData['theme'] === 'dark' ? 'sun' : 'moon' }}"></i></a></li>
+  {{-- <li class="nav-item d-none d-lg-block"><a class="nav-link nav-link-style"><i class="ficon"
+        data-feather="{{ $configData['theme'] === 'dark' ? 'sun' : 'moon' }}"></i></a></li> --}}
 </ul>

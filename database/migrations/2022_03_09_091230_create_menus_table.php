9904eeb3ae267efac870323a98e93f33e5a6ac05
ajcastro <ajcastro29@gmail.com>
2022-03-22 06:57:28 +0800

    Add menu migration

---
+++ database/migrations/2022_03_09_091230_create_menus_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMenusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('menus', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->foreignId('parent_id');
+            $table->unsignedTinyInteger('sort_order')->default(0);
+            $table->string('slug');
+            $table->string('imgloc');
+            $table->boolean('is_active')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('menus');
+    }
+}

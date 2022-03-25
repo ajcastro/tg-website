c8ab46e1708a9d46a8189d357c8b66138bd742a7
ajcastro <ajcastro29@gmail.com>
2022-03-25 11:59:42 +0800

    Set autoplay slider

--- resources/views/website/landing-page/slider.blade.php
+++ resources/views/website/landing-page/slider.blade.php
@@ -56,10 +56,10 @@
         spaceBetween: 10,
         grabCursor: true,
         loop:true,
-        // autoplay: {
-        //     delay: 5000,
-        //     disableOnInteraction: false,
-        // },
+        autoplay: {
+            delay: 5000,
+            disableOnInteraction: false,
+        },
         pagination: {
             el: ".swiper-pagination",
             clickable: true,

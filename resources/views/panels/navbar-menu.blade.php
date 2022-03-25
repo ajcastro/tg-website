000797343cc14471f0e938b4473aed3b01b5a749
ajcastro <ajcastro29@gmail.com>
2022-03-25 11:52:50 +0800

    Apply d-none d-md-inline-flex in navbar-menu to hide navbar-menu in mobile screen

--- resources/views/panels/navbar-menu.blade.php
+++ resources/views/panels/navbar-menu.blade.php
@@ -1,4 +1,4 @@
-<ul class="nav navbar-nav navbar-menu align-items-center ms-auto">
+<ul class="nav navbar-nav navbar-menu align-items-center ms-auto d-none d-md-inline-flex">
   @each('panels.navbar-submenu', App\Models\Menu::tree(), 'menu')
 
   {{-- <li class="nav-item d-none d-lg-block"><a class="nav-link nav-link-style"><i class="ficon"

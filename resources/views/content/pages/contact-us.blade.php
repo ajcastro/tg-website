33d85df74f50c15edb3620c43355e92669764233
ajcastro <ajcastro29@gmail.com>
2022-04-21 12:31:22 +0800

    Improve contact us

--- resources/views/content/pages/contact-us.blade.php
+++ resources/views/content/pages/contact-us.blade.php
@@ -2,13 +2,16 @@
 <div class="customizer d-none d-md-block">
 
   <a class="customizer-toggle d-flex align-items-center justify-content-center" href="javascript:void(0);">
-    <i data-feather='phone-call'></i>
+    <div class="bottom-to-top center fs-lg" style="margin-right: 8px;">
+      <i data-feather='phone-call'></i>
+       &nbsp;CONTACT US &nbsp;
+    </div>
   </a>
 
   <div class="customizer-content">
     <!-- Customizer header -->
     <div class="customizer-header px-2 pt-1 pb-0 position-relative">
-      <h4 class="mb-0">Contact Us</h4>
+      <h3 class="mb-0">Contact Us</h3>
       {{-- <p class="m-0">Customize & Preview in Real Time</p> --}}
       <a class="customizer-close" href="javascript:void(0);"><i data-feather="x"></i></a>
     </div>
@@ -16,7 +19,7 @@
     <hr style="margin-bottom: 0px;" />
 
     <!-- Contact Us Items -->
-    <div class="contact-us-items p-2">
+    <div class="contact-us-items p-2" style="font-size: 16px;">
       <div class="list-group">
         @foreach (\App\Models\ContactSetting::onlyDisplayable()->forCurrentWebsite()->get() as $contact)
         <a href="{{ $contact->value }}" class="list-group-item list-group-item-action" target="_blank"> {{ $contact->title }} </a>
